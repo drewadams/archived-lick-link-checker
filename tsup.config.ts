d1173a063eed058057f48e5984fbd2d40f152708
Drew <drewadams044@gmail.com>
2023-09-19 13:28:46 -0400

    Add lib to compiler

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["src/index.ts", "src/lib/functions.ts"],
+	entry: ["src/"],
 	sourcemap: true,
 	clean: true,
 	outDir: "bin",
